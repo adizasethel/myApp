c10e80e3c5029b22abe209d867a177e18b713ac0
adizasethel <ethel.adizas@evsu.edu.ph>
2021-06-01 12:00:58 +0800

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+ firebaseConfig :{
+  apiKey: "AIzaSyA2v2MOgIn3XprppzQTSpnS-mQJhsBb0IU",
+  authDomain: "myapp-b6711.firebaseapp.com",
+  databaseURL: "https://myapp-b6711-default-rtdb.firebaseio.com",
+  projectId: "myapp-b6711",
+  storageBucket: "myapp-b6711.appspot.com",
+  messagingSenderId: "792211217752",
+  appId: "1:792211217752:web:e3965f6eca23b0dd4c192d",
+  measurementId: "G-V96JWLDG4J"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+  apiKey: "AIzaSyA2v2MOgIn3XprppzQTSpnS-mQJhsBb0IU",
+  authDomain: "myapp-b6711.firebaseapp.com",
+  databaseURL: "https://myapp-b6711-default-rtdb.firebaseio.com",
+  projectId: "myapp-b6711",
+  storageBucket: "myapp-b6711.appspot.com",
+  messagingSenderId: "792211217752",
+  appId: "1:792211217752:web:e3965f6eca23b0dd4c192d",
+  measurementId: "G-V96JWLDG4J"
+}
 };
 
 /*
